aa0caaa58fd0ccb2b62eaf32cb61064bee284b72
Student <student@lab-315-028.cs.csubak.edu>
2017-03-02 09:38:42 -0800

    adding christianC.cpp
    Former-commit-id: 05656dfb6259f43be282bf911df2164fde9066e4

--- christianC.cpp
+++ christianC.cpp
@@ -1,8 +1,170 @@
 #include <iostream>
 using namespace std;
 
-void printChristian()
+extern void printChristian()
 {
-	cout << "Christian\n";
+    cout << "This is Christian\n";
+    return;
+}
+
+
+
+
+/*
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+#include <cmath>
+#include <X11/Xlib.h>
+#include <X11/keysym.h>
+#include <GL/glx.h>
+#include "./libraries/fonts.h"
+
+#define WINDOW_WIDTH  800
+#define WINDOW_HEIGHT 600
+//X Windows variables
+Display *dpy;
+Window win;
+GLXContext glc;
+//Structures
+struct Vec {
+	float x, y, z;
+};
+
+struct Shape{
+    float width, height;
+    Vec center;
+};
+
+struct Menu {
+	Shape menu;
+};
+
+//Function prototypes
+void initXWindows(void);
+void init_opengl(void);
+void cleanupXWindows(void);
+void render(Menu *myMenu);
+extern void buildMenu();
+
+extern void buildMenu()
+{
+	int done=0;
+	srand(time(NULL));
+	initXWindows();
+	init_opengl();
+	//declare game object
+	Menu myMenu;
+
+	//declare a menu
+	myMenu.menu.width = 200;
+	myMenu.menu.height = 150;
+	myMenu.menu.center.x = 400;
+	myMenu.menu.center.y = 300;
+
+	//start animation
+	while (!done) {
+		while (XPending(dpy)) {
+			XEvent e;
+			XNextEvent(dpy, &e);
+		}
+		render(&myMenu);
+		glXSwapBuffers(dpy, win);
+	}
+	cleanupXWindows();
 	return;
 }
+
+void set_title(void)
+{
+	//Set the window title bar.
+	XMapWindow(dpy, win);
+	XStoreName(dpy, win, "DODGE!");
+}
+
+void cleanupXWindows(void)
+{
+	//do not change
+	XDestroyWindow(dpy, win);
+	XCloseDisplay(dpy);
+}
+
+void initXWindows(void)
+{
+	//do not change
+	GLint att[] = { GLX_RGBA, GLX_DEPTH_SIZE, 24, GLX_DOUBLEBUFFER, None };
+	int w=WINDOW_WIDTH, h=WINDOW_HEIGHT;
+	dpy = XOpenDisplay(NULL);
+	if (dpy == NULL) {
+		std::cout << "\n\tcannot connect to X server\n" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	Window root = DefaultRootWindow(dpy);
+	XVisualInfo *vi = glXChooseVisual(dpy, 0, att);
+	if (vi == NULL) {
+		std::cout << "\n\tno appropriate visual found\n" << std::endl;
+		exit(EXIT_FAILURE);
+	} 
+	Colormap cmap = XCreateColormap(dpy, root, vi->visual, AllocNone);
+	XSetWindowAttributes swa;
+	swa.colormap = cmap;
+	swa.event_mask = ExposureMask | KeyPressMask | KeyReleaseMask |
+		ButtonPress | ButtonReleaseMask | PointerMotionMask |
+		StructureNotifyMask | SubstructureNotifyMask;
+	win = XCreateWindow(dpy, root, 0, 0, w, h, 0, vi->depth,
+		InputOutput, vi->visual, CWColormap | CWEventMask, &swa);
+	set_title();
+	glc = glXCreateContext(dpy, vi, NULL, GL_TRUE);
+	glXMakeCurrent(dpy, win, glc);
+}
+
+void init_opengl(void)
+{
+	//OpenGL initialization
+	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+	//Initialize matrices
+	glMatrixMode(GL_PROJECTION); glLoadIdentity();
+	glMatrixMode(GL_MODELVIEW); glLoadIdentity();
+	//Set 2D mode (no perspective)
+	glOrtho(0, WINDOW_WIDTH, 0, WINDOW_HEIGHT, -1, 1);
+	//Set the screen background color
+	glClearColor(0.1, 0.1, 0.1, 1.0);
+	//Set text
+	glEnable(GL_TEXTURE_2D);
+	initialize_fonts();
+}
+
+void render(Menu *myMenu)
+{
+	float w, h;
+	glClear(GL_COLOR_BUFFER_BIT);
+	//Draw shapes...
+
+	//draw box
+	Shape *s;
+	glColor3ub(49, 86, 145);
+	s = &myMenu->menu;
+	glPushMatrix();
+	glTranslatef(s->center.x, s->center.y, s->center.z);
+	w = s->width;
+	h = s->height;
+	glBegin(GL_QUADS);
+		glVertex2i(-w,-h);
+		glVertex2i(-w, h);
+		glVertex2i( w, h);
+		glVertex2i( w,-h);
+		glEnd();
+		glPopMatrix();
+	Rect r;
+	r.bot = s->center.y + 80;
+	r.left = s->center.x;
+	r.center = s->center.y;
+	unsigned int color = 0xc13c47;
+	ggprint13(&r, 10, color, "Welcome to the DODGE game menu!");
+	glEnd();
+
+}
+*/
+
+
