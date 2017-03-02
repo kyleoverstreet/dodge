20cb0670d2a9b3e6a5c6a1be03935c86f345e808
kory <korydespot@yahoo.com>
2017-03-01 21:35:49 -0800

    changes
    Former-commit-id: 3e245cb3d2bee0f76606ba4777076ff1c0e77945

--- koryD.cpp
+++ koryD.cpp
@@ -2,11 +2,18 @@
 
 #include <iostream>
 #include <AL/al.h>
+#include <string.h>
 #include <AL/alc.h>
 
-ALCdevice *device;
 
-device = alcOpenDevice(NULL);
+#ifdef LIBAUDIO
+#include <audio/wave.h>
+#define BACKEND "libaudio"
+#else
+#include <AL/alut.h>
+#define BACKEND "alut"
+#endif
+
 
 using namespace std;
 
@@ -16,4 +23,21 @@ void kory()
 	cout << "This is a test of kory's function\n";
 	return;
 }
+
+
+static void list_audio_devices(const ALCchar *devices)
+{
+	const ALCchar *device = devices, *next = devices + 1;
+	size_t len = 0;
+	
+	while (device && *device != '\0' && next && *next != '\0') 
+	{
+		fprintf(stdout, "%s\n", device);
+		len = strlen(device);
+		device += (len + 1);
+		next += (len + 2);
+	}
+}
+
+
 
