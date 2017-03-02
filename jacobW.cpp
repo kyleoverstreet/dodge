74ac41e99ad343dff11927d0e12744974d0d342a
Student <student@lab-315-034.cs.csubak.edu>
2017-03-02 09:21:30 -0800

    Changes 3/2
    Former-commit-id: 0b41db0c1227afe740ae5fa74a22a151454fd0b2

--- jacobW.cpp
+++ jacobW.cpp
@@ -1,68 +1,62 @@
 #include <iostream>
 #include <cstring>
+#include <math.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <time.h>
+#include <X11/Xlib.h>
+#include <X11/keysym.h>
+#include <GL/glx.h>
+#include "./libraries/ppm.h"
+
 using namespace std;
 
-/*void cleanupPPM(void) {
-    system("rm ./images/standL.ppm");
-    system("rm ./images/PixelBG.ppm");
-}
+#define MakeVector(x, y, z, v) (v)[0]=(x), (v)[1]=(y), (v)[2]=(z)
+#define VecCopy(a,b) (b)[0]=(a)[0];(b)[1]=(a)[1];(b)[2]=(a)[2]
 
+typedef double Flt;
+typedef double Vec[3];
 typedef struct t_player {
     Vec pos;
-    Ved vel;
+    Vec vel;
     Vec lastpos;
 } Player;
-Player player;
 
-void movePlayer(int xres) {
+void cleanupPPM(void) {
+    system("rm ./images/standL.ppm");
+    system("rm ./images/PixelBG.ppm");
+    system("rm ./images/StandR.ppm");
+}
 
+void movePlayer(int xres, Player *player) {
     //Check if edge left
-    if (player.pos[0] <= 0) {
-	player.pos[0] = 0;
+    if (player->pos[0] <= 40) {
+	player->pos[0] = 40;
     }
 
     //Check if edge right
-    if (player.pos[0] >= xres) {
-	player.pos[0] = xres;
+    if (player->pos[0] >= xres-40) {
+	player->pos[0] = xres-40;
     }
 }
 
-void checkKeys(XEvent *e) {
-    static int shift=0;
-    int key = XLookupKeysym(&e->xkey, 0);
-    if (e->type == KeyRelease) {
-	if (key == XK_Shift_L || key == XK_Shift_R)
-	    shift = 0;
-	return;
-    }
-    if (e->type == KeyPress) {
-	if (key == XK_Shift_L || key == XK_Shift_R) {
-	    shift = 1;
-	    return;
-	}
-    } else {
-	return;
-    }
-    switch(key) {
-	case XK_b:
-	    //toggle character
-	    break;
-	case XK_Left:
-	    //VecCopy(bigfoot.pos, bigfoot.lastpos);
-	    player.pos[0] -= 8.0;
-	case XK_Right:
-	    //VecCopy(bigfoot.pos, bigfoot.lastpos);
-	    player.pos[0] += 8.0;
-
-void physics(void) {
-    
-    //This is defined above for boundaries
-    movePlayer();
-    
-    //This will be for checking the items falling
-    //checkItems();
+void init(int xres, int yres, Player *player) {
+    player->pos[0] = xres/2;
+    player->pos[1] = yres/920;
+    VecCopy(player->pos, player->lastpos);
+    MakeVector(-150.0, 180.0, 0.0, player->pos);
+    MakeVector(5.0,0.0,0.0, player->vel);
 }
-*/
+
+void keypressL(Player *player) {
+    player->pos[0] -= 8.0;
+}
+
+void keypressR(Player *player) {
+    player->pos[0] += 8.0;
+}
+
 void jacob () {
     cout << "This is Jacob's test function" << endl;
     return;
