6c100b15c96bccd5f5f3d0f4417a2fce48924c68
kyoverstreet <kyoverstreet@gmail.com>
2017-03-02 08:35:58 -0800

    Edited upload_scores function call
    Former-commit-id: f69c6919343953eb71fb679fb991915566eb138e

--- dodge.cpp
+++ dodge.cpp
@@ -4,9 +4,8 @@ extern void upload_scores(const char*);
 
 int main()
 {
-    //Kyle's function - uploads scores to webpage
-    const char *file_path = "/home/stu/koverstreet/public_html/3350/dodge.html";
-    upload_scores(file_path);
+    // Kyle's function
+    upload_scores("scores.html");
     
     printChristian();
     jacob();
