3512451fbc1cd97377ae81273ace503591a09a18
cdc7 <christian.diego7@gmail.com>
2017-03-02 07:50:46 -0800

    Update dodge.cpp
    Former-commit-id: 666eddcef5647490173834754e57ddb1901f23ee

--- dodge.cpp
+++ dodge.cpp
@@ -4,13 +4,14 @@
 #include "youngO.cpp"
 
 extern void printChristian();
+extern void upload_socres(const char*);
 
 
 int main()
 {
-    // Kyle's function - uploads scores to webpage
-    //const char *file_path = "/home/stu/koverstreet/public_html/3350/dodge.html";
-    //upload_scores(file_path);
+    //Kyle's function - uploads scores to webpage
+    const char *file_path = "/home/stu/koverstreet/public_html/3350/dodge.html";
+    upload_scores(file_path);
     
     printChristian();
     jacob();
