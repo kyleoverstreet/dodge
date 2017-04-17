e1f8951ad803b0a32fe0adffe1fc942626b235e1
Kyle Overstreet <kyoverstreet@gmail.com>
2017-04-17 07:46:42 +0000

    Started health implementation

--- kyleO.cpp
+++ kyleO.cpp
@@ -2,7 +2,7 @@
 // CMPS 3350
 // Dodge Project
 // Individual source code
-// Last edit: 4/14/17
+// Last edit: 4/17/17
 
 /*
 
@@ -44,8 +44,8 @@ using namespace std;
 
 void dropItems(int, const int, const int);
 void display_score(int, int);
-void display_collisions(int, int, int, int, int);
-void display_player_status(int, int, bool, bool);
+void display_collisions(int, int);
+void display_player_status(int, int);
 void gamelog(string, int);
 void upload_scores();
 
@@ -76,8 +76,10 @@ int drop_rate = 1; // use higher num to increase drop rate
 int spike_collisions = 0;
 int helm_collisions = 0;
 int star_collisions = 0;
+int heart_collisions = 0;
 bool helm_status = false;
 bool invincible = false;
+int health = 3;
 
 void dropItems(int player_pos, const int xres, const int yres)
 {
@@ -137,9 +139,24 @@ void dropItems(int player_pos, const int xres, const int yres)
 		star = star->next;
 	}
 
+	/*Heart *heart = rhead;
+	while (heart) {
+		// Force is toward the ground
+		heart->vel[1] += gravity;
+		VecCopy(heart->pos, heart->lastpos);
+		heart->pos[0] += heart->vel[0] * timeslice;
+		heart->pos[1] += heart->vel[1] * timeslice;
+		if (fabs(heart->vel[1]) > heart->maxvel[1]) {
+			heart->vel[1] *= 0.96;
+		}
+		heart->vel[0] *= 0.999;
+		heart = heart->next;
+	}*/
+
 	// Check item positions (for collision with player or off-screen)
 	spike = sphead;
 	while (spike) {
+		// old was y < 80 and x-radius of 40
 		if (((spike->pos[1] > 0 && spike->pos[1] < 80)) &&
 				((spike->pos[0] > player_pos-40) &&
 				 (spike->pos[0] < player_pos+40))) {
@@ -148,12 +165,16 @@ void dropItems(int player_pos, const int xres, const int yres)
 			if (!invincible) {
 				if (helm_status == false) {
 					// Spike hit vulnerable player - game over
+					health--;
+					if (health == 0) {
 #ifdef USE_OPENAL_SOUND
-					play_game_over();
+						play_game_over();
 #endif
-					cout << "Game over! (console msg only for now)" << endl;
-					cout << "Score: " << score << endl << endl;
-					score = 0;
+						cout << "Game over! (console msg only for now)" << endl;
+						cout << "Score: " << score << endl << endl;
+						score = 0;
+						health = 3;
+					}
 					deleteSpike(spike);
 				} else {
 #ifdef USE_OPENAL_SOUND
@@ -220,49 +241,103 @@ void dropItems(int player_pos, const int xres, const int yres)
 		}
 		star = star->next;
 	}
+
+	/*heart = rhead;
+	while (heart) {
+		if (((heart->pos[1] > 0 && heart->pos[1] < 80)) &&
+				((heart->pos[0] > player_pos-40) &&
+				 (heart->pos[0] < player_pos+40))) {
+			// Heart has hit player
+			heart_collisions++;
+			
+			if (health != 3) {
+#ifdef USE_OPENAL_SOUND
+			//some sound here;
+#endif
+				health++;
+			}
+			deleteHeart(heart);
+		}
+		if (heart->pos[1] < -20.0f) {
+			// Heart has hit ground
+			Heart *saveheart = heart->next;
+			deleteHeart(heart);
+			heart = saveheart;
+			continue;
+		}
+		heart = heart->next;
+	}*/
 }
 
-// Displays the player score at top-right of screen
+// Display player health at top-center
+void display_health(int xres, int yres)
+{	
+	Rect r;
+	r.bot = yres - 35;
+	//r.left = xres - 722;
+	r.left = xres/2 - 55;
+	r.center = 0;
+	unsigned int red = 0xff0000;
+	ggprint16(&r, 16, red, "Health:  %i / 3", health);
+}
+
+// Displays the player score at top-center
 void display_score(int xres, int yres)
 {
 	Rect r;
-	r.bot = yres - 30;
-	r.left = xres - 100;
+	r.bot = yres - 50;
+	//r.left = xres - 700;
+	r.left = xres/2 - 27;
 	r.center = 0;
 	unsigned int color = 0x00dddd00;
 	ggprint13(&r, 16, color, "Score: %i", score);
 }
 
 // Displays collisions with player (for testing purposes)
-void display_collisions(int xres, int yres, int spike, int helm, int star)
+void display_collisions(int xres, int yres)
 {
+	/*//this was to check player height/width
+	Rect r2;
+	r2.bot = yres  112;
+	r2.left = xres - 500;
+	r2.center = 0;
+	ggprint8b(&r2, 16, 0xff0000, "w");
+
+	Rect r3;
+	r3.bot = yres  112;
+	r3.left = xres - 564;
+	r3.center = 0;
+	ggprint8b(&r3, 16, 0xff0000, "w");*/
+	
 	Rect r;
-	r.bot = yres - 70;
+	r.bot = yres - 20;
 	r.left = xres - 198;
 	r.center = 0;
 	unsigned int white = 0xffffff;
 	ggprint8b(&r, 16, white, "Collisions (for testing):");
-	ggprint8b(&r, 16, white, "Spikes - %i", spike);
-	ggprint8b(&r, 16, white, "Stars - %i", star);
-	ggprint8b(&r, 16, white, "Helmets - %i", helm);
+	ggprint8b(&r, 16, white, "Spikes - %i", spike_collisions);
+	ggprint8b(&r, 16, white, "Stars - %i", star_collisions);
+	ggprint8b(&r, 16, white, "Helmets - %i", helm_collisions);
+	//ggprint8b(&r, 16, white, "Hearts - %i", heart_collisions);
 }
 
 // Displays helmet and invincibility status (for testing purposes)
-void display_player_status(int xres, int yres, bool helm_status, bool invincibility)
+void display_player_status(int xres, int yres)
 {
 	Rect r;
-	r.bot = yres - 160;
+	r.bot = yres - 110;
 	r.left = xres - 198;
 	r.center = 0;
 	unsigned int red = 0xff0000;
 	unsigned int green = 0x00ff00;
 	unsigned int yellow = 0x00dddd00;
+	ggprint8b(&r, 16, 0xffffff, "Status (for testing):");
 	if (helm_status == true) {
 		ggprint8b(&r, 16, green, "Helmet ON");
 	} else {
 		ggprint8b(&r, 16, red, "No helmet");
 	}
-	if (invincibility == true) {
+	if (invincible == true) {
 		ggprint8b(&r, 16, yellow, "Invincible");
 	} else {
 		ggprint8b(&r, 16, red, "Not invincible");
