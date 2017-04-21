d030c21114a2704a130767f2b63920c2aa6485e6
Kyle Overstreet <kyoverstreet@gmail.com>
2017-04-21 05:40:50 +0000

    player2 implementation (not yet working)

--- jacobW.cpp
+++ jacobW.cpp
@@ -13,57 +13,58 @@
 using namespace std;
 
 void cleanupPPM(void) {
-    system("rm ./images/background1.ppm");
-    system("rm ./images/standL.ppm");
-    system("rm ./images/standhelmL.ppm");
-    system("rm ./images/starplayer.ppm");
-    system("rm ./images/Spike.ppm");
-    system("rm ./images/helmet.ppm");
-    system("rm ./images/Star.ppm");
+	system("rm ./images/background1.ppm");
+	system("rm ./images/standL.ppm");
+	system("rm ./images/standhelmL.ppm");
+	system("rm ./images/starplayer.ppm");
+	system("rm ./images/player2.ppm");
+	system("rm ./images/Spike.ppm");
+	system("rm ./images/helmet.ppm");
+	system("rm ./images/Star.ppm");
 }
 
 int movePlayer(int xres, Player *player) {
 
-    player->pos[0] += player->vel[0];
+	player->pos[0] += player->vel[0];
 
-    //Check if edge left
-    if (player->pos[0] <= 40) {
-        player->pos[0] = 40;
-        player->vel[0] = 0;
-    }
+	//Check if edge left
+	if (player->pos[0] <= 40) {
+		player->pos[0] = 40;
+		player->vel[0] = 0;
+	}
 
-    //Check if edge right
-    if (player->pos[0] >= xres-40) {
-        player->pos[0] = xres-40;
-        player->vel[0] = 0;
-    }
+	//Check if edge right
+	if (player->pos[0] >= xres-40) {
+		player->pos[0] = xres-40;
+		player->vel[0] = 0;
+	}
 
-    if (player->vel[0] < -3) { 
-        player->vel[0] += 2;
-    } else if (player->vel[0] > 3) {
-        player->vel[0] += -2;
-    } else if (player->vel[0] <= 3 && player->vel[0] >= -3) {
-        player->vel[0] = 0;
-    }
+	if (player->vel[0] < -3) { 
+		player->vel[0] += 2;
+	} else if (player->vel[0] > 3) {
+		player->vel[0] += -2;
+	} else if (player->vel[0] <= 3 && player->vel[0] >= -3) {
+		player->vel[0] = 0;
+	}
 
-    //return player's x-position (needed to detect collisions)
-    return player->pos[0];	
+	//return player's x-position (needed to detect collisions)
+	return player->pos[0];	
 }
 
 void init(int xres, int yres, Player *player) {
-    player->pos[0] = xres/2;
-    player->pos[1] = yres/920;
-    VecCopy(player->pos, player->lastpos);
-    MakeVector(-150.0, 180.0, 0.0, player->pos);
-    MakeVector(0.0,0.0,0.0, player->vel);
+	player->pos[0] = xres/2;
+	player->pos[1] = yres/920;
+	VecCopy(player->pos, player->lastpos);
+	MakeVector(-150.0, 180.0, 0.0, player->pos);
+	MakeVector(0.0,0.0,0.0, player->vel);
 }
 
 void keypressL(Player *player) {
-    player->vel[0] -= 3.5;
-    player->LR = false;
+	player->vel[0] -= 3.5;
+	player->LR = false;
 }
 
 void keypressR(Player *player) {
-    player->vel[0] += 3.5;
-    player->LR = true;
+	player->vel[0] += 3.5;
+	player->LR = true;
 }
